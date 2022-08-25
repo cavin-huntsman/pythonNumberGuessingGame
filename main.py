5b904e981981f275ae70f91d28934a72d6fd1c92
cavin-huntsman <cavinhuntsman@gmail.com>
2022-08-25 15:53:25 -0600

    updated game

--- main.py
+++ main.py
@@ -6,25 +6,9 @@ def main():
     methods.startGame()
     lives = methods.setLives()
     num = methods.setNumber()
-#    guess = methods.getNumber()
     methods.compareNumber(num, lives)
     time.sleep(1)
 
 
 while True:
-    main()
-
-#def main():
-#    print('__________Welcome to "Guess That Number!"__________')
-#    while True:
-#        games = input('Enter 1 to play - Enter 0 to quit')
-#        games = int(games)
-#        if games == 1:
-#            number_guess()
-#        elif games == 0:
-#            break
-#    print('__________Thanks for playing!__________')
-
-
-if __name__ == "__main__":
     main()
