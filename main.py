b10a824cae59611cb3ed24088bcb4df1f74348db
cavin-huntsman <cavinhuntsman@gmail.com>
2022-08-26 01:09:58 -0600

    - added clarification text to setNumber method - changed wait time from 2s to 1.5s

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import time
 
 
 def main():
-    methods.startGame()
-    num = methods.setNumber()
-    lives = methods.setLives()
-    methods.compareNumber(num, lives)
+    methods.startGame()  # do you want to start yes or no?
+    methods.play()  # play the game
     time.sleep(1.5)
 
 
 while True:
     main()
+
+
+#def main2():
+#    methods.startGame()
+#    mode = methods.chooseMode()
+#    num = methods.setNumber()
+#    round = 1
+#    lives = methods.setLives()
+#    methods.compareNumber(num, lives, mode, round)
+#    time.sleep(1.5)
